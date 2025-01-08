9d82daecffb558df1da98bd357d289081c515c16
Antti Nurmi <antti.nurmi@tuni.fi>
2025-01-08 13:11:10 +0200

    Start over Verilator

--- verilator/rt_top_unpacked.cpp
+++ verilator/rt_top_unpacked.cpp
@@ -2,42 +2,87 @@
 #include <memory>
 #include <stdio.h>
 
+#define CLK_PER      4
+#define RST_DELAY    80
+#define CLK_DELAY    20
+#define JTAG_START   120
+#define JTAG_CLK_PER 5
+
 #include "Vrt_top_unpacked.h"
 #include "verilated_fst_c.h"
 #include "verilated.h"
-#include "SimCxt.h"
-#include "vip/src/ClkRstDrv.h"
+#include "vip/src/testbench.h"
+//#include "SimCxt.h"
+//#include "vip/src/ClkRstDrv.h"
+//#include "vip/src/JtagDrv.h"
+
+//enum Sequence {
+//  JTAG,
+//  IDLE,
+//  DONE
+//};
+
+class TbRtTop : public Testbench<Vrt_top_unpacked> {
+
+};
 
 int main(int argc, char** argv) {
 
   Verilated::commandArgs(argc, argv);
-  Verilated::traceEverOn(true);
-  vluint64_t sim_time = 0;
-  SimCtx cx(new Vrt_top_unpacked, new VerilatedFstC, sim_time);
+  //Verilated::traceEverOn(true);
+
+  TbRtTop* tb = new TbRtTop();
+
+  tb->open_trace("./waveform.fst");
+  for (int it=0;it<10;it++)
+    tb->tick();
+
+  tb->reset();
+
+  for (int it=0;it<100;it++) tb->tick();
+  //uint64_t sim_time = 0;
+  //SimCtx cx(new Vrt_top_unpacked, new VerilatedFstC, sim_time);
+  //enum Sequence seq = JTAG;
   
-  cx.dut->trace(cx.trace, 5);
-  cx.trace->open("./waveform.fst");
-  //const std::unique_ptr<Vrt_top_unpacked> top{new Vrt_top_unpacked};
-
-  ClkRstDrv* cr_drv = new ClkRstDrv(&cx);
-
-  timestep(&cx);
-  timestep(&cx);
-  timestep(&cx);
-
-  //timestep(&cx);
-  printf("Hello there\n");
-  //while ( !Verilated::gotFinish() ) { 
-  //  if(main_time != -1) {
-  //    top->eval();    
-  //  }
-  //  main_time = top->nextTimeSlot();
-  //}
+  //cx.dut->trace(cx.trace, 5);
+  //cx.trace->open("./waveform.fst");
 //
-  //top->final();
+  //ClkRstDrv* cr_drv = new ClkRstDrv(&cx);
+  //JtagDrv* jtag_drv = new JtagDrv(&cx, JTAG_CLK_PER);
 
-  cx.trace->close();
-  delete cr_drv;
+  //int runtime=0;
+  //while(runtime < 5000) {
+  //  bool after_re = (cx.dut->clk_i  && cx.dut->rst_ni &&
+  //                  (sim_time % (CLK_PER*2) == 0));
+  //  cr_drv->clock(CLK_DELAY, CLK_PER);
+  //  cr_drv->reset(RST_DELAY);
+//
+  //  // drive inputs with slight delay after RE
+  //  if (after_re) {
+  //    switch (seq)
+  //    {
+  //    case JTAG:
+  //      if(jtag_drv->connectivity_test(&cx, JTAG_START))
+  //        seq = IDLE;
+  //      break;
+//
+  //    case IDLE: 
+  //      printf("DONE\n");
+  //      seq = DONE; 
+  //      break;
+  //    default: break;
+  //    }
+  //  }
+//
+  //  // TODO: Update sequence state
+//
+  //  cx.timestep();
+  //  runtime++;
+  //}
+
+  delete tb;
+  //cx.trace->close();
+  //delete cr_drv;
 
   return 0;
 }
---
+++ verilator/vip/src/testbench.h
@@ -0,0 +1,67 @@
+// Testbench template, modelled from https://github.com/ZipCPU/zipcpu/blob/master/sim/verilator/testb.h
+
+template <class VA>
+class Testbench {
+public:
+    VA             *m_dut;
+    VerilatedFstC*  m_trace;
+    uint64_t        m_tickcount;
+
+    Testbench(void) : m_trace(NULL), m_tickcount(01) {
+        m_dut = new VA;
+        Verilated::traceEverOn(true);
+		m_dut->clk_i = 0;
+		eval(); // set initial values
+    }
+
+    virtual ~Testbench(void){
+        close_trace();
+        delete m_dut;
+        m_dut = NULL;
+    }
+
+    virtual void open_trace(const char* fst_name){
+        if (!m_trace) {
+            m_trace = new VerilatedFstC;
+            m_dut->trace(m_trace, 99);
+            m_trace->open(fst_name);
+        }
+    }
+
+    virtual void close_trace(void) {
+        if (m_trace){
+            m_trace->close();
+            delete m_trace;
+            m_trace = NULL;
+        }
+    }
+    
+    virtual void eval(void) {
+        m_dut->eval();
+    }
+
+    virtual void tick(void) {
+        m_tickcount++;
+        eval();
+        if (m_trace) m_trace->dump((vluint64_t)(10*m_tickcount-2));
+        m_dut->clk_i = 1;
+        eval();
+        if (m_trace) m_trace->dump((vluint64_t)(10*m_tickcount));
+        m_dut->clk_i = 0;
+        eval();
+        if (m_trace){
+            m_trace->dump((vluint64_t)(10*m_tickcount+5));
+            m_trace->flush();
+        }
+    }
+
+    virtual void reset(void) {
+        m_dut->rst_ni = 0;
+        tick();
+        m_dut->rst_ni = 1;
+    }
+
+    uint64_t tickcount(void) {
+		return m_tickcount;
+	}
+};
