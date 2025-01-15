4d59d76518241b7b24d03c09681f0424eff99ff5
Antti Nurmi <antti.nurmi@tuni.fi>
2025-01-15 11:30:49 +0200

    Add support for Makefile-supplied test string

--- verilator/rt_top_unpacked.cpp
+++ verilator/rt_top_unpacked.cpp
@@ -2,6 +2,10 @@
 #include <memory>
 #include <stdio.h>
 
+// preprocessor hack to deal with strings
+#define xstr(s) str(s)
+#define str(s) #s
+
 #define CLK_PER      4
 #define RST_DELAY    80
 #define CLK_DELAY    20
@@ -15,16 +19,20 @@ extern "C" char read_elf(const char *filename);
 #include "verilated.h"
 #include "vip/src/Testbench.h"
 
-
 class TbRtTop : public Testbench<Vrt_top_unpacked> {};
 
 int main(int argc, char** argv) {
 
   Verilated::commandArgs(argc, argv);
   TbRtTop* tb = new TbRtTop();
+  std::string test = xstr(TEST);
 
   tb->open_trace("./waveform.fst");
   for (int it=0;it<100;it++) tb->tick();
+
+  if (test == "none") {
+    printf("TEST not set, exiting\n");
+  } else {
 
   tb->reset();
   tb->jtag_reset_master();
@@ -32,8 +40,7 @@ int main(int argc, char** argv) {
   tb->jtag_memory_test();
   tb->jtag_load_elf();
   tb->jtag_wait_eoc();
-
-
+  }
 
   delete tb;
 
