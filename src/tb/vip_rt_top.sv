f0ba11907e6ff33d37df2b83a34e4975c99ec515
Antti Nurmi <antti.nurmi@tuni.fi>
2025-01-22 17:13:18 +0200

    Fix hardcoded entry

--- src/tb/vip_rt_top.sv
+++ src/tb/vip_rt_top.sv
@@ -303,7 +303,8 @@ task automatic jtag_elf_halt_load(input string binary, output word entry);
   while (~status.allhalted);
   $display("[JTAG] Halted hart 0");
   // Preload binary
-  jtag_elf_preload(binary, entry);
+  //jtag_elf_preload(binary, entry);
+  jtag_elf_preload("/home/antti/uart-halfbaud", entry);
 endtask
 
 
@@ -319,7 +320,7 @@ task automatic jtag_elf_run(input string binary);
   word entry;
   jtag_elf_halt_load(binary, entry);
   // Repoint execution
-  write_reg_abstract_cmd(dm::CSR_DPC, rt_pkg::ImemRule.Start);
+  write_reg_abstract_cmd(dm::CSR_DPC, entry);
   // Resume hart 0
   jtag_write(dm::DMControl, dm::dmcontrol_t'{resumereq: 1, dmactive: 1, default: '0});
   $display("[JTAG] Resumed hart 0 from 0x%h", entry);
