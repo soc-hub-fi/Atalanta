be083d0e79aace013eb2aeff5444e0aeed0fe203
Henri Lunnikivi <heggggxa@gmail.com>
2025-02-02 19:03:11 +0200

    fix: skip callee save for PCS trampoline

--- examples/atalanta_bsp/macros/src/lib.rs
+++ examples/atalanta_bsp/macros/src/lib.rs
@@ -23,24 +23,10 @@ pub fn nested_interrupt_riscv32i(args: TokenStream, input: TokenStream) -> Token
 
 #[proc_macro]
 pub fn generate_continue_nested_trap_riscv32e(_input: TokenStream) -> TokenStream {
-    const NO_HW_STACK: bool = false;
-    generate_continue_nested_trap(RiscvArch::Rv32E, NO_HW_STACK)
+    generate_continue_nested_trap(RiscvArch::Rv32E)
 }
 
 #[proc_macro]
 pub fn generate_continue_nested_trap_riscv32i(_input: TokenStream) -> TokenStream {
-    const NO_HW_STACK: bool = false;
-    generate_continue_nested_trap(RiscvArch::Rv32I, NO_HW_STACK)
-}
-
-#[proc_macro]
-pub fn generate_continue_nested_pcs_trap_riscv32e(_input: TokenStream) -> TokenStream {
-    const USE_HW_STACK: bool = true;
-    generate_continue_nested_trap(RiscvArch::Rv32E, USE_HW_STACK)
-}
-
-#[proc_macro]
-pub fn generate_continue_nested_pcs_trap_riscv32i(_input: TokenStream) -> TokenStream {
-    const USE_HW_STACK: bool = true;
-    generate_continue_nested_trap(RiscvArch::Rv32I, USE_HW_STACK)
+    generate_continue_nested_trap(RiscvArch::Rv32I)
 }
