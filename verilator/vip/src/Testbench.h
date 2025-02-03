2e31f904256965d19eb86c00cd31222984df3acb
Henri Lunnikivi <heggggxa@gmail.com>
2025-02-03 14:19:13 +0200

    sim: set waveform clock freq to 100 MHz
    This seems to give correct numbers in gtkwave.

--- verilator/vip/src/Testbench.h
+++ verilator/vip/src/Testbench.h
@@ -3,7 +3,7 @@
 #include <cstdint>
 
 // Clock period in picoseconds, used for generating waveforms
-const uint32_t CLOCK_PERIOD_PS = /* 50 MHz */ 20'000;
+const uint32_t CLOCK_PERIOD_PS = /* 100 MHz */ 10'000;
 
 template <class VA>
 class Testbench {
