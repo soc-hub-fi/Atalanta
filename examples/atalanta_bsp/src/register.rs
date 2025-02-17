fd1d8bb3e88b564a8d2e0bae6abb3a4664a339b6
Henri Lunnikivi <heggggxa@gmail.com>
2025-02-17 16:03:48 +0200

    bsp: reorder register.rs

--- examples/atalanta_bsp/src/register.rs
+++ examples/atalanta_bsp/src/register.rs
@@ -29,60 +29,6 @@ pub mod mconfigptr {
     read_csr_as_usize!(0xF15);
 }
 
-pub mod mintthresh {
-    use riscv::{clear, read_csr_as, set, write_csr_as};
-
-    /// mintthresh register
-    ///
-    /// Holds an 8-bit field (th) for the threshold level of the associated
-    /// privilege mode. The th field is held in the least-significant 8 bits of
-    /// the CSR, and zero should be written to the upper bits.
-    ///
-    /// A typical usage of the interrupt-level threshold is for implementing
-    /// critical sections. The current handler can temporarily raise its
-    /// effective interrupt level to implement a critical section among a subset
-    /// of levels, while still allowing higher interrupt levels to preempt.
-    ///
-    /// The current hart’s effective interrupt level would then be:
-    /// effective_level = max(mintstatus.mil`, mintthresh.th)
-    ///
-    /// The max is used to prevent a hart from dropping below its original level
-    /// which would break assumptions in design, and also makes it simple for
-    /// software to remove threshold without knowing its own level by simply
-    /// setting mintthresh to the lowest supported mintthresh value.
-    ///
-    /// The interrupt-level threshold is only valid when running in associated
-    /// privilege mode and not in other modes. This is because interrupts for
-    /// lower privilege modes are always disabled, whereas interrupts for higher
-    /// privilege modes are always enabled.
-    #[derive(Clone, Copy)]
-    #[cfg_attr(feature = "ufmt", derive(crate::ufmt::derive::uDebug))]
-    #[cfg_attr(not(feature = "ufmt"), derive(Debug))]
-    pub struct Mintthresh {
-        bits: usize,
-    }
-
-    impl From<usize> for Mintthresh {
-        #[inline]
-        fn from(bits: usize) -> Self {
-            Self { bits }
-        }
-    }
-
-    impl Mintthresh {
-        /// Returns the contents of the register as raw bits
-        #[inline]
-        pub fn bits(&self) -> usize {
-            self.bits
-        }
-    }
-
-    read_csr_as!(Mintthresh, 0x347);
-    write_csr_as!(Mintthresh, 0x347);
-    set!(0x347);
-    clear!(0x347);
-}
-
 pub mod mtvt {
     use riscv::{clear, read_csr_as, set, write_csr};
 
@@ -249,6 +195,60 @@ pub mod mintstatus {
     }
 
     read_csr_as!(Mintstatus, 0x346);
+}
+
+pub mod mintthresh {
+    use riscv::{clear, read_csr_as, set, write_csr_as};
+
+    /// mintthresh register
+    ///
+    /// Holds an 8-bit field (th) for the threshold level of the associated
+    /// privilege mode. The th field is held in the least-significant 8 bits of
+    /// the CSR, and zero should be written to the upper bits.
+    ///
+    /// A typical usage of the interrupt-level threshold is for implementing
+    /// critical sections. The current handler can temporarily raise its
+    /// effective interrupt level to implement a critical section among a subset
+    /// of levels, while still allowing higher interrupt levels to preempt.
+    ///
+    /// The current hart’s effective interrupt level would then be:
+    /// effective_level = max(mintstatus.mil`, mintthresh.th)
+    ///
+    /// The max is used to prevent a hart from dropping below its original level
+    /// which would break assumptions in design, and also makes it simple for
+    /// software to remove threshold without knowing its own level by simply
+    /// setting mintthresh to the lowest supported mintthresh value.
+    ///
+    /// The interrupt-level threshold is only valid when running in associated
+    /// privilege mode and not in other modes. This is because interrupts for
+    /// lower privilege modes are always disabled, whereas interrupts for higher
+    /// privilege modes are always enabled.
+    #[derive(Clone, Copy)]
+    #[cfg_attr(feature = "ufmt", derive(crate::ufmt::derive::uDebug))]
+    #[cfg_attr(not(feature = "ufmt"), derive(Debug))]
+    pub struct Mintthresh {
+        bits: usize,
+    }
+
+    impl From<usize> for Mintthresh {
+        #[inline]
+        fn from(bits: usize) -> Self {
+            Self { bits }
+        }
+    }
+
+    impl Mintthresh {
+        /// Returns the contents of the register as raw bits
+        #[inline]
+        pub fn bits(&self) -> usize {
+            self.bits
+        }
+    }
+
+    read_csr_as!(Mintthresh, 0x347);
+    write_csr_as!(Mintthresh, 0x347);
+    set!(0x347);
+    clear!(0x347);
 }
 
 // mscratchsw, mscratchswl are useful for multi-privilege only
