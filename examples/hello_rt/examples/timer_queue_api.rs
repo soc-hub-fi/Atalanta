59d90190b8111602b5f6c45dfbc49983e0004520
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2025-08-05 16:58:35 +0300

    HACK(sw(example)): timer queue API (broken)

---
+++ examples/hello_rt/examples/timer_queue_api.rs
@@ -0,0 +1,78 @@
+//! UART pin map, incl. PYNQ-Z1:
+//!
+//! | Host  | Device    | PYNQ-Z1 |
+//! | :-:   | :-:       | :-:     |
+//! | TX    | RX        | IO12    |
+//! | RX    | TX        | IO13    |
+#![no_main]
+#![no_std]
+#![allow(non_snake_case)]
+
+use bsp::{
+    clic::Clic, nested_interrupt, riscv, rt::entry, sprint, sprintln, timer_queue::TimerQueue,
+    uart::*, Interrupt, CPU_FREQ,
+};
+use hello_rt::{print_example_name, setup_irq, tear_irq, UART_BAUD};
+
+static mut IS_FULL: bool = false;
+
+#[entry]
+fn main() -> ! {
+    let mut serial = ApbUart::init(CPU_FREQ, UART_BAUD);
+    print_example_name!();
+
+    sprintln!("Setup CLIC...");
+    // Set level bits to 8
+    Clic::smclicconfig().set_mnlbits(8);
+
+    // Enable global interrupts
+    unsafe { riscv::interrupt::enable() };
+
+    setup_irq(Interrupt::TqFull);
+    setup_irq(Interrupt::TqNotFull);
+    sprintln!("done");
+
+    let timer_q = TimerQueue::init();
+
+    // Push 8 values. Should result in 'full' interrupt.
+    let mut handles = [0; 8];
+    for idx in 0..8 {
+        let ts = idx as u64 + 1;
+        let pl = (8 - idx as u8) + 1;
+        handles[idx] = timer_q.push_abs(ts, pl);
+        sprintln!("Push {}, {} -> {}", ts, pl, handles[idx]);
+    }
+
+    // 8 values => queue is full
+    assert!(unsafe { IS_FULL });
+
+    sprintln!("Drop {}", handles[0]);
+    timer_q.drop(handles[0]);
+
+    // 7 values => queue is not full
+    assert!(unsafe { !IS_FULL });
+
+    sprintln!("Top: {}", timer_q.top_idx());
+
+    sprintln!("Tear down");
+
+    tear_irq(Interrupt::TqFull);
+    tear_irq(Interrupt::TqNotFull);
+    riscv::interrupt::disable();
+
+    bsp::tb::signal_pass(Some(&mut serial));
+
+    loop {}
+}
+
+#[nested_interrupt]
+fn TqFull() {
+    sprintln!("TQ is full (irq)");
+    unsafe { IS_FULL = true };
+}
+
+#[nested_interrupt]
+fn TqNotFull() {
+    sprintln!("TQ is not full (irq)");
+    unsafe { IS_FULL = false };
+}
