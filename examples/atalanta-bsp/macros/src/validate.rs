1d2582144c0ad4624d26e056650498e40801e894
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2025-08-07 19:26:42 +0300

    sw(bsp): validate irq handler names

--- examples/atalanta-bsp/macros/src/validate.rs
+++ examples/atalanta-bsp/macros/src/validate.rs
@@ -2,6 +2,21 @@ use proc_macro::TokenStream;
 use proc_macro2::Span;
 use syn::{parse, spanned::Spanned, ItemFn, ReturnType, Type, Visibility};
 
+/// List of platform interrupts. Should match with what's found in
+/// [atalanta-bsp/src/interrupts.rs].
+#[rustfmt::skip]
+const INTERRUPTS: &[&str] = &[
+    "MachineSoft", "MachineTimer", "MachineExternal",
+    "Uart", "Gpio",
+    "SpiRxTxIrq", "SpiEotIrq",
+    "Timer0Ovf", "Timer0Cmp", "Timer1Ovf", "Timer1Cmp", "Timer2Ovf", "Timer2Cmp", "Timer3Ovf", "Timer3Cmp",
+    "TqFull", "TqNotFull",
+    "Nmi",
+    "Dma0", "Dma1", "Dma2", "Dma3", "Dma4", "Dma5", "Dma6", "Dma7",
+    "Dma8", "Dma9", "Dma10", "Dma11", "Dma12", "Dma13", "Dma14", "Dma15",
+    "TqId0", "TqId1", "TqId2", "TqId3", "TqId4", "TqId5", "TqId6", "TqId7"
+];
+
 /// Returns possible errors with the interrupt handler definition
 pub(crate) fn validate_interrupt_handler(args: TokenStream, f: &ItemFn) -> Option<TokenStream> {
     // check the function arguments
@@ -10,6 +25,21 @@ pub(crate) fn validate_interrupt_handler(args: TokenStream, f: &ItemFn) -> Optio
             parse::Error::new(
                 f.sig.inputs.first().unwrap().span(),
                 "`#[nested_interrupt]` function should not have arguments",
+            )
+            .to_compile_error()
+            .into(),
+        );
+    }
+
+    // check that the supplied identified is one of known interrupt handlers
+    let ident = &f.sig.ident;
+    let export_name = format!("{:#}", ident);
+    let valid_ident = INTERRUPTS.contains(&export_name.as_str());
+    if !valid_ident {
+        return Some(
+            parse::Error::new(
+                f.sig.ident.span(),
+                "`#[nested_interrupt]` function must have identifier that matches with one of platform interrupts in atalanta_bsp::interrupt::Interrupt",
             )
             .to_compile_error()
             .into(),
