106eb701a1f04e3d225451edae62e1b70ca13cd4
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2025-08-19 16:32:28 +0300

    bench(pqbench): use cp for rotate script

--- scripts/rotate-measurements.sh
+++ scripts/rotate-measurements.sh
@@ -4,13 +4,14 @@
 set -e
 
 # -Fuse-bheap
-mv logs/bheap.log logs/bheap-frozen.log
+cp logs/bheap.log logs/bheap-frozen.log
 
 # -Fuse-imap
-mv logs/imap.log logs/imap-frozen.log
+cp logs/imap.log logs/imap-frozen.log
 
 # -Fuse-hwq
-mv logs/hwq.log logs/hwq-frozen.log
+cp logs/hwq.log logs/hwq-frozen.log
 
-mv logs/virtq.log logs/virtq-frozen.log
+# -Fuse-hwq -Fuse-virtq
+cp logs/virtq.log logs/virtq-frozen.log
 
