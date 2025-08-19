157c76c00ab8231cf36b0af7a5c88be8296c6fe3
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2025-08-19 16:32:18 +0300

    bench(pqbench): use tee for measure scripts

--- scripts/measure-novirt.sh
+++ scripts/measure-novirt.sh
@@ -14,17 +14,17 @@ make verilate
 mkdir -p logs
 
 # -Fuse-bheap
-make verilate simv RUST=1 TEST_DIR=examples/pqbench TEST=pqprof TARGET="riscv32imc-unknown-none-elf" CARGO_FLAGS="-Fuse-bheap" > logs/bheap.log
-ls -lah examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof >> logs/bheap.log
-size -A -d examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof >> logs/bheap.log
+make verilate simv RUST=1 TEST_DIR=examples/pqbench TEST=pqprof TARGET="riscv32imc-unknown-none-elf" CARGO_FLAGS="-Fuse-bheap" | tee logs/bheap.log
+ls -lah examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof | tee -a logs/bheap.log
+size -A -d examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof | tee -a logs/bheap.log
 
 # -Fuse-imap
-make verilate simv RUST=1 TEST_DIR=examples/pqbench TEST=pqprof TARGET="riscv32imc-unknown-none-elf" CARGO_FLAGS="-Fuse-imap" > logs/imap.log
-ls -lah examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof >> logs/imap.log
-size -A -d examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof >> logs/imap.log
+make verilate simv RUST=1 TEST_DIR=examples/pqbench TEST=pqprof TARGET="riscv32imc-unknown-none-elf" CARGO_FLAGS="-Fuse-imap" | tee logs/imap.log
+ls -lah examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof | tee -a logs/imap.log
+size -A -d examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof | tee -a logs/imap.log
 
 # -Fuse-hwq
-make verilate simv RUST=1 TEST_DIR=examples/pqbench TEST=pqprof TARGET="riscv32imc-unknown-none-elf" CARGO_FLAGS="-Fuse-hwq" > logs/hwq.log
-ls -lah examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof >> logs/hwq.log
-size -A -d examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof >> logs/hwq.log
+make verilate simv RUST=1 TEST_DIR=examples/pqbench TEST=pqprof TARGET="riscv32imc-unknown-none-elf" CARGO_FLAGS="-Fuse-hwq" | tee logs/hwq.log
+ls -lah examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof | tee -a logs/hwq.log
+size -A -d examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof | tee -a logs/hwq.log
 
--- scripts/measure-virt.sh
+++ scripts/measure-virt.sh
@@ -14,7 +14,7 @@ make verilate
 mkdir -p logs
 
 # -Fuse-hwq -Fvirtq
-make verilate simv RUST=1 TEST_DIR=examples/pqbench TEST=pqprof TARGET="riscv32imc-unknown-none-elf" CARGO_FLAGS="-Fuse-hwq -Fvirtq" > logs/virtq.log
-ls -lah examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof >> logs/virtq.log
-size -A -d examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof >> logs/virtq.log
+make verilate simv RUST=1 TEST_DIR=examples/pqbench TEST=pqprof TARGET="riscv32imc-unknown-none-elf" CARGO_FLAGS="-Fuse-hwq -Fvirtq" | tee logs/virtq.log
+ls -lah examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof | tee -a logs/virtq.log
+size -A -d examples/pqbench/target/riscv32imc-unknown-none-elf/release/examples/pqprof | tee -a logs/virtq.log
 
