d6473eefeb96979d67f458ffe5d96fec746f078c
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2025-09-19 10:43:47 +0300

    demo: replace HW impl TimerQ with 'todo'

--- examples/pqbench/src/hwq.rs
+++ examples/pqbench/src/hwq.rs
@@ -5,16 +5,16 @@ impl PQueue for bsp::timer_queue::TimerQueue {
 
     #[inline(always)]
     fn enqueue_rel(&mut self, entry: Self::Entry) -> u8 {
-        bsp::riscv::interrupt::free(|| (self as &mut Self).push_rel(entry))
+        bsp::riscv::interrupt::free(|| todo!())
     }
 
     #[inline(always)]
     fn enqueue_abs(&mut self, entry: Self::Entry) -> u8 {
-        bsp::riscv::interrupt::free(|| (self as &mut Self).push_abs(entry))
+        bsp::riscv::interrupt::free(|| todo!())
     }
 
     #[inline(always)]
     fn drop(&mut self, handle: u8) {
-        bsp::riscv::interrupt::free(|| (self as &mut Self).drop(handle));
+        bsp::riscv::interrupt::free(|| todo!());
     }
 }
